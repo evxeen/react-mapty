bfdaba3d6477271e4f744c68380d252f96993ebd
evxeen <evxeen@gmail.com>
2021-10-27 17:59:47 +0300

    feature: get the user's location

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import Map from "./components/Map/Map";
 import {Sidebar} from "./components/Sideber/Sidebar";
 
 function App() {
-  return (
+
+  const [coords, setCoords] = useState([]);
+
+  const getPosition = async function() {
+    if (navigator.geolocation) {
+      const data = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject);
+      })
+      setCoords([data.coords.latitude, data.coords.longitude])
+    }
+  }
+
+  getPosition();
+
+ return (
     <div className="wrapper">
       <Sidebar />
-      <Map id="map" />
+      {coords.length > 0 ? <Map id="map" coords={coords}/> : null}
     </div>
   );
 }
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,21 +1,21 @@
 import React from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 import "./Map.css";
 
-function Map(props) {
-  return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
-  );
+function Map({coords}) {
+    return (
+        <MapContainer center={coords} zoom={12} scrollWheelZoom={true}>
+          <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={coords}>
+            <Popup>
+              A pretty CSS3 popup. <br/> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
+    )
 }
 
 export default Map;
